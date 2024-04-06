7cc95223c891c62cf3617616aa92e9ce7a8c62ba
KeerthanaReddy-4 <reddykeertana2004@gmail.com>
2024-04-06 10:44:53 +0530

    message

---
+++ myapp/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+         <h1> This is my Reactjs Application</h1>    
+          
+             <li> Hyd</li>
+             <li> Delhi</li>
+             <li> Vizag</li>
+             <li> Chennai</li>
+            
+    </div>
+  );
+}
+
+export default App;
